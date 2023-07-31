3ccc33ea968af6b90269abc7274a71b284bc6103
Anthony Ruiz <yobakaj@gmail.com>
2023-07-31 17:43:02 -0400

    route done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBNavbarBrand,
+  MDBCollapse
+} from 'mdb-react-ui-kit';
+import "./App.css";
+import Home from './components/Home';
+import Red from './components/Red';
+import Blue from './components/Blue';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [showNavColor, setShowNavColor] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MDBNavbar expand='lg' dark bgColor='primary'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>Route</MDBNavbarBrand>
+          <MDBNavbarToggler
+            type='button'
+            data-target='#navbarColor02'
+            aria-controls='navbarColor02'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowNavColor(!showNavColor)}
+          >
+            <MDBIcon icon='bars' fas />
+          </MDBNavbarToggler>
+          <MDBCollapse show={showNavColor} navbar>
+            <MDBNavbarNav className='me-auto mb-2 mb-lg-0'>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='/'>Home</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='/blue'>Blue</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='/red'>Red</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+      <div id="main-section">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+          </Routes>
+        </div>
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText
+} from 'mdb-react-ui-kit';
+
+export default function Home() {
+  return (
+    <>
+      <MDBCard>
+        <MDBCardBody>
+        <MDBCardTitle><h1 className='card-header-home'>Home</h1></MDBCardTitle>
+          <MDBCardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Maecenas velit dui, pharetra eleifend condimentum id, eleifend vel lectus. 
+            Vestibulum congue sem sit amet rutrum mollis. 
+            Nunc consectetur ipsum tortor, eu mollis neque euismod eu.
+          </MDBCardText>
+        </MDBCardBody>
+      </MDBCard>
+    </>
+  );
+}
